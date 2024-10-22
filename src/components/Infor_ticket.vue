1b8197f3841903ec56be0502cea5a7758e41e2ca
ThanhTung-2010 <dtong201003@gmail.com>
2024-10-22 17:23:05 +0700

    man hinh gia ve

--- src/components/Infor_ticket.vue
+++ src/components/Infor_ticket.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        <h1>Info-Ticket</h1>
+    <div class="background">
+        <div class="container">
+            <h1>Thông tin giá vé</h1>
+            <img src="../assets/giave.jpg" alt="Ticket Price" class="ticket-price-image" />
+            <p>Chính thức cập nhật giá vé xem phim dành riêng các cán bộ, công chức, viên chức Nhà nước khi đến xem phim
+                tại LOTTE Cinema WestLake, cụ thể</p>
+            <p style="font-weight: bold;">* 85.000 VNĐ/vé từ T2-T6</p>
+            <p style="font-weight: bold;">* 105.000đ VNĐ/vé cho T7 & CN</p>
+            <p>*Ghi chú:</p>
+            <p>- Giá vé trên áp dụng cho các cán bộ, công chức, viên chức và người lao động thuộc các cơ quan Nhà nước
+            </p>
+            <p>- Đặc biệt, khi đi cùng người thân, bạn bè theo nhóm (tối đa 4 người), tất cả sẽ đều được áp dụng mức giá
+                vé đặc biệt trên.</p>
+            <p>- Quý khách hàng vui lòng xuất trình thẻ cán bộ, công chức, viên chức (thẻ giấy hoặc thẻ điện tử) trực
+                tiếp tại quầy khi mua vé.</p>
+        </div>
     </div>
 </template>
 
@@ -10,6 +24,29 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+p {
+    text-align: left;
+    margin: 10px;
+}
 
+.background {
+    background-color: #F9F6EC;
+    padding: 20px;
+    text-align: center;
+}
+
+.ticket-price-image {
+    max-width: 100%;
+    height: auto;
+    display: block;
+    margin: 20px auto;
+
+}
+
+.container {
+    width: 70%;
+    padding: 10px;
+    background-color: white;
+}
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,32 +3,21 @@
     <img src="../assets/logomenu.png" alt="Logo" class="menu-logo" />
     <v-divider style="margin-bottom: 20px;"></v-divider>
     <ul>
-        <li><router-link
-        to="/home-page"
-        active-class="active-link"
-        exact-active-class="exact-active-link">
-        <span class="fa-solid fa-house"></span>Trang chủ</router-link></li>
-            <hr class="dash-lined" style="margin-top: 20px; margin-bottom: 20px; color: grey;">
-        <li><router-link
-        to="/favorite-film"
-        active-class="active-link"
-        exact-active-class="exact-active-link">
-        <span class="fa-solid fa-star"></span>Phim yêu thích</router-link></li>
-            <hr class="dash-lined" style="margin-top: 20px; margin-bottom: 20px;">
-        <li><router-link
-        to="/ticket"
-        active-class="active-link"
-        exact-active-class="exact-active-link">
-        <span class="fa-solid fa-ticket"></span>Vé đã mua</router-link></li>
-            <hr class="dash-lined" style="margin-top: 20px; margin-bottom: 20px;">
-        <li><router-link
-        to="/infor-tiket"
-        active-class="active-link"
-        exact-active-class="exact-active-link">
-        <span class="fa-solid fa-coins"></span>Thông tin giá vé</router-link></li>
-            <hr class="dash-lined" style="margin-top: 20px; margin-bottom: 20px;">
-        <li class="nav-logout">
-        <button class="icon-button" style="color: red; justify-content: left; white-space: nowrap;" @click="$emit('handle-logout')"><span class="fa-solid fa-sign-out-alt"></span>Đăng xuất</button>
+      <li><router-link to="/film" active-class="active-link" exact-active-class="exact-active-link">
+          <span class="fa-solid fa-house"></span>Trang chủ</router-link></li>
+      <hr class="dash-lined" style="margin-top: 20px; margin-bottom: 20px; color: grey;">
+      <li><router-link to="/favorite-film" active-class="active-link" exact-active-class="exact-active-link">
+          <span class="fa-solid fa-star"></span>Phim yêu thích</router-link></li>
+      <hr class="dash-lined" style="margin-top: 20px; margin-bottom: 20px;">
+      <li><router-link to="/ticket" active-class="active-link" exact-active-class="exact-active-link">
+          <span class="fa-solid fa-ticket"></span>Vé đã mua</router-link></li>
+      <hr class="dash-lined" style="margin-top: 20px; margin-bottom: 20px;">
+      <li><router-link to="/infor-tiket" active-class="active-link" exact-active-class="exact-active-link">
+          <span class="fa-solid fa-coins"></span>Thông tin giá vé</router-link></li>
+      <hr class="dash-lined" style="margin-top: 20px; margin-bottom: 20px;">
+      <li class="nav-logout">
+        <button class="icon-button" style="color: red; justify-content: left; white-space: nowrap;"
+          @click="$emit('handle-logout')"><span class="fa-solid fa-sign-out-alt"></span>Đăng xuất</button>
       </li>
     </ul>
   </div>
@@ -57,22 +46,29 @@ export default {
   box-shadow: -2px 0 5px rgba(0, 0, 0, 0.1);
   padding: 20px;
   z-index: 1000;
-  transform: translateX(100%); /* Ẩn menu bằng cách dịch chuyển nó ra khỏi màn hình */
-  transition: transform 0.3s; /* Thêm hiệu ứng chuyển động cho menu */
+  transform: translateX(100%);
+
+  transition: transform 0.3s;
+
 }
+
 .menu ul {
   list-style-type: none;
   padding: 0;
   justify-content: left;
 }
+
 .menu-open .menu {
-  transform: translateX(0); /* Hiển thị menu bằng cách dịch chuyển nó vào màn hình */
+  transform: translateX(0);
+
 }
+
 .menu-logo {
   width: 30%;
   height: auto;
   margin-bottom: 20px;
 }
+
 .active-link {
   color: rgb(0, 157, 255) !important;
 }
@@ -80,20 +76,28 @@ export default {
 .exact-active-link {
   color: #1db4f0 !important;
 }
+
 .active-link i,
 .exact-active-link i {
   color: #1db4f0 !important;
 }
-.menu ul li a  {
-    text-decoration: none;
-    color:black;
+
+.menu ul li a {
+  text-decoration: none;
+  color: black;
 }
+
+.menu ul li a:hover {
+  color: #1db4f0;
+}
+
 .menu ul li {
-    text-decoration: none;
-    text-align: left;
-    font-size: 20px;
+  text-decoration: none;
+  text-align: left;
+  font-size: 20px;
 }
+
 .menu ul li span {
-    margin-right: 10px;
+  margin-right: 10px;
 }
 </style>
